fe1043ac31f76f17c77ca4399fd1272f5db8062c
Ogre <theogreman@gmail.com>
2014-07-28 16:41:32 +0100

    init

---
+++ app.py
@@ -0,0 +1,85 @@
+from __future__ import print_function
+
+from datetime import datetime, timedelta
+from twittcher import SearchWatcher
+from celery import Celery
+from celery.task import periodic_task
+
+import tweepy
+import os
+import operator
+import re
+
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_KEY  = os.environ['ACCESS_KEY']
+ACCESS_SECRET = os.environ['ACCESS_SECRET']
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+REDIS_URL = os.environ.get('REDISTOGO_URL', 'redis://localhost')
+celery = Celery(__name__, broker=REDIS_URL)
+
+REMOVE_LIST = ["-", "a", "an", "as", "at", "before", "but", "by", "for","from","is", "in", "into", "like", "of", "off", "on", "onto","per","since", "than", "the", "this", "that", "to", "up", "via","with"]
+
+
+class TopTweetWatcher(SearchWatcher):
+
+    def __init__(self, search_term, action=print, database=None):
+        SearchWatcher.__init__(self, search_term, action, database)
+        self.lex = {}
+
+    def calculate_most_used(self):
+        for tweet in self.seen_tweets:
+            for word in tweet.text.split():
+                word = word.lower()
+                if word.startswith('#'):
+                    word = word[1:]
+                elif word.startswith('@'):
+                    continue
+                elif re.match('^[\w-]+$', word) is None:
+                    continue
+                elif word in REMOVE_LIST:
+                    continue
+                if word in self.lex:
+                    self.lex[word] += 1
+                else:
+                    self.lex[word] = 1
+
+    def get_most_popular(self, num=1):
+        if not self.lex:
+            self.calculate_most_used()
+        return list(
+            item for item, _ in sorted(
+                self.lex.iteritems(),
+                key=operator.itemgetter(1),
+                reverse=True
+            )
+        )[:num]
+
+
+bot = TopTweetWatcher("bristol")
+
+
+@periodic_task(run_every=timedelta(minutes=5))
+def check_tweets():
+    bot.watch()
+
+
+@periodic_task(run_every=timedelta(minutes=10))
+def check_top():
+    bot.calculate_most_used()
+
+
+@periodic_task(run_every=timedelta(minutes=30))
+def tweet_top():
+    for words in range(14, 0, -1):
+        status = ' '.join(bot.get_most_popular(words))
+        if len(status) <= 140:
+            api.update_status(status=status)
+            break
+
